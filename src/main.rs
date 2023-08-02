ad1d214bd6ff21f85746b7d8d95a3d41b11e467f
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-08-02 20:59:11 +0200

    refactor(main): remove unneeded types

--- src/main.rs
+++ src/main.rs
@@ -22,18 +22,16 @@ mod units;
 use std::sync::Arc;
 
 use anyhow::{Context, Result};
-use bundle_aggregator::SlotBundle;
 use futures::{
-    channel::mpsc::{self, Receiver, Sender},
+    channel::mpsc::{self},
     try_join,
 };
 use health::{MessageConsumerHealth, NatsHealth};
 use tracing::info;
 
 use crate::{
-    bundle_aggregator::BundleAggregator,
-    bundle_shipper::BundleShipper,
-    message_consumer::{AckablePayload, MessageConsumer},
+    bundle_aggregator::BundleAggregator, bundle_shipper::BundleShipper,
+    message_consumer::MessageConsumer,
 };
 
 #[derive(Debug, Clone)]
@@ -59,14 +57,12 @@ async fn main() -> Result<()> {
     let message_health = Arc::new(MessageConsumerHealth::new());
 
     // Ackable payload queue
-    type MessageQueue = (Sender<AckablePayload>, Receiver<AckablePayload>);
-    let (ackable_payload_tx, ackable_payload_rx): MessageQueue = mpsc::channel(512);
+    let (ackable_payload_tx, ackable_payload_rx) = mpsc::channel(512);
     let message_consumer =
         MessageConsumer::new(message_health.clone(), nats_client, ackable_payload_tx).await;
 
     // Bundle queue
-    type BundleQueue = (Sender<SlotBundle>, Receiver<SlotBundle>);
-    let (bundle_tx, bundle_rx): BundleQueue = mpsc::channel(16);
+    let (bundle_tx, bundle_rx) = mpsc::channel(16);
     let bundle_aggregator = BundleAggregator::new(bundle_tx);
 
     let object_store = object_stores::build_env_based_store(env_config)?;
