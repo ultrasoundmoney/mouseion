f37cdf8baaa10f56999bfd78881521c878c83f41
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-08-07 21:00:17 +0200

    Revert "refactor(trace_memory): remove module"
    This reverts commit ba24b3387a388a2f230b267ee385385f3c8a3498.

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ mod object_stores;
 mod operation_constants;
 mod performance;
 mod server;
+mod trace_memory;
 
 use std::sync::Arc;
 
@@ -26,7 +27,7 @@ use futures::channel::mpsc::{self};
 use health::{MessageConsumerHealth, NatsHealth};
 use payload_archiver::env;
 use tokio::{sync::Notify, try_join};
-use tracing::info;
+use tracing::{info, Level};
 
 use crate::{
     bundle_aggregator::BundleAggregator, bundle_shipper::BundleShipper,
@@ -54,6 +55,17 @@ async fn main() -> Result<()> {
     info!("starting payload archiver");
 
     let shutdown_notify = Arc::new(Notify::new());
+
+    if tracing::enabled!(Level::TRACE) {
+        let handle = tokio::spawn(async move {
+            trace_memory::report_memory_periodically().await;
+        });
+        let shutdown_notify = shutdown_notify.clone();
+        tokio::spawn(async move {
+            shutdown_notify.notified().await;
+            handle.abort();
+        });
+    }
 
     let env_config = &env::ENV_CONFIG;
 
---
+++ src/trace_memory.rs
@@ -0,0 +1,22 @@
+use std::time::Duration;
+
+use memory_stats::memory_stats;
+use tokio::task::JoinHandle;
+use tracing::trace;
+
+pub async fn report_memory_periodically() -> JoinHandle<()> {
+    let mut interval = tokio::time::interval(Duration::from_secs(3));
+
+    loop {
+        interval.tick().await;
+
+        if let Some(usage) = memory_stats() {
+            trace!(
+                mem_mb = usage.physical_mem / 1_000_000,
+                "current physical memory usage",
+            );
+        } else {
+            println!("couldn't get the current memory usage");
+        }
+    }
+}
