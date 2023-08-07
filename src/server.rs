8a0ed8ac38d30ee03f926c0d9ec1bd3d6eb466ed
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-08-07 10:56:38 +0200

    feat(server): use localhost to avoid firewall

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
 use anyhow::Context;
 use axum::{routing::get, Router, Server};
+use payload_archiver::env::{Env, ENV_CONFIG};
 use tokio::sync::Notify;
 use tracing::{error, info};
 
@@ -11,9 +12,18 @@ pub async fn serve(state: AppState, shutdown_notify: &Notify) {
             .route("/livez", get(get_livez))
             .with_state(state);
 
+        let address = match ENV_CONFIG.env {
+            // This avoids macOS firewall popups when developing locally.
+            Env::Dev => "127.0.0.1",
+            Env::Stag => "0.0.0.0",
+            Env::Prod => "0.0.0.0",
+        };
+
         let port = env::get_env_var("PORT").unwrap_or_else(|| "3003".to_string());
-        info!(port, "server listening");
-        let socket_addr = format!("0.0.0.0:{port}").parse().unwrap();
+
+        info!(address, port, "server listening");
+
+        let socket_addr = format!("{address}:{port}").parse().unwrap();
 
         Server::bind(&socket_addr)
             .serve(app.into_make_service())
