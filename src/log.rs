4579a7a1b52fc7ed6a855f6e8ede18c1b4468f84
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-09-08 14:13:27 +0200

    feat(log): print span duration on LOG_PERF

--- src/log.rs
+++ src/log.rs
@@ -1,14 +1,22 @@
-use tracing_subscriber::EnvFilter;
+use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
 
 use crate::env::{Env, ENV_CONFIG};
 
 pub fn init() {
+    let span_format = if ENV_CONFIG.log_perf {
+        FmtSpan::CLOSE
+    } else {
+        FmtSpan::NONE
+    };
+
     if ENV_CONFIG.env == Env::Dev {
         tracing_subscriber::fmt()
+            .with_span_events(span_format)
             .with_env_filter(EnvFilter::from_default_env())
             .init();
     } else {
         tracing_subscriber::fmt()
+            .with_span_events(span_format)
             .with_env_filter(EnvFilter::from_default_env())
             .json()
             .init();
