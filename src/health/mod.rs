dda91900ecffdf37aab2c8adbca2c400d1d34603
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-09-08 15:47:53 +0200

    fix(health): redis not nats

--- src/health/mod.rs
+++ src/health/mod.rs
@@ -15,20 +15,26 @@ trait HealthCheck {
 }
 
 pub async fn get_livez(State(state): State<AppState>) -> impl IntoResponse {
-    let (is_nats_healthy, nats_health_status) = state.redis_health.health_status();
+    let (is_redis_healthy, redis_health_status) = state.redis_health.health_status();
     // We check but don't count message health. Messages should be continuously coming in on
     // production. Until we know whether that is reliable we don't want to fail the health
     // check.
     let (_is_messages_healthy, messages_health_status) =
         state.redis_consumer_health.health_status();
 
-    let message = json!({ "nats": nats_health_status, "messages": messages_health_status });
+    let message = json!({ "redis": redis_health_status, "messages": messages_health_status });
 
-    if is_nats_healthy {
-        debug!(nats = nats_health_status, messages = messages_health_status);
+    if is_redis_healthy {
+        debug!(
+            redis = redis_health_status,
+            messages = messages_health_status
+        );
         (StatusCode::OK, Json(message))
     } else {
-        warn!(nats = nats_health_status, messages = messages_health_status);
+        warn!(
+            redis = redis_health_status,
+            messages = messages_health_status
+        );
         (StatusCode::SERVICE_UNAVAILABLE, Json(message))
     }
 }
